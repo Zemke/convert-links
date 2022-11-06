f7ec9f6ca3a937c2ea630c098a5b7eb944d23d8f
Zemke <florian@zemke.io>
2022-11-06 18:31:48 +0100

    Multiple updates
    - Mutliple subdomains
    - Forgot about Safari
    - Protocol is now a prerequisite
    - Allow small subdomains
    - Lookbehind now supported by Firefox

--- test.js
+++ test.js
@@ -18,60 +18,61 @@ const expect = expected => ({
 });
 
 it('containing nothing more than a link', () => {
-  expect(convertLinks("www.cwtsite.com"))
-    .toEqual('<a href="http://www.cwtsite.com" target="_blank">www.cwtsite.com</a>')
+    expect(convertLinks("http://www.cwtsite.com"))
+        .toEqual('<a href="http://www.cwtsite.com" target="_blank">http://www.cwtsite.com</a>')
 });
 
 it('with a link surrounded by strings.', () => {
-  expect(convertLinks("The greatest page in the world is www.cwtsite.com I swear!"))
-    .toEqual('The greatest page in the world is <a href="http://www.cwtsite.com" target="_blank">www.cwtsite.com</a> I swear!')
+    expect(convertLinks("The greatest page in the world is http://www.cwtsite.com I swear!"))
+        .toEqual('The greatest page in the world is <a href="http://www.cwtsite.com" target="_blank">http://www.cwtsite.com</a> I swear!')
 });
 
 it('containing http or www or https.', () => {
-  expect(convertLinks("A page called http://cwtsite.com is a nice page."))
-    .toEqual('A page called <a href="http://cwtsite.com" target="_blank">http://cwtsite.com</a> is a nice page.',
-      'http failed');
+    expect(convertLinks("A page called http://cwtsite.com is a nice page."))
+        .toEqual('A page called <a href="http://cwtsite.com" target="_blank">http://cwtsite.com</a> is a nice page.',
+            'http failed');
 
-  expect(convertLinks("A page called www.cwtsite.com is a nice page."))
-    .toEqual('A page called <a href="http://www.cwtsite.com" target="_blank">www.cwtsite.com</a> is a nice page.',
-      'www failed');
+    expect(convertLinks("A page called https://cwtsite.com is a nice page."))
+        .toEqual('A page called <a href="https://cwtsite.com" target="_blank">https://cwtsite.com</a> is a nice page.',
+            'https failed');
 
-  expect(convertLinks("A page called https://cwtsite.com is a nice page."))
-    .toEqual('A page called <a href="https://cwtsite.com" target="_blank">https://cwtsite.com</a> is a nice page.',
-      'https failed');
+    expect(convertLinks("A page called http://www.cwtsite.com is a nice page."))
+        .toEqual('A page called <a href="http://www.cwtsite.com" target="_blank">http://www.cwtsite.com</a> is a nice page.',
+            'http and www failed');
 
-  expect(convertLinks("A page called http://www.cwtsite.com is a nice page."))
-    .toEqual('A page called <a href="http://www.cwtsite.com" target="_blank">http://www.cwtsite.com</a> is a nice page.',
-      'http and www failed');
-
-  expect(convertLinks("A page called https://www.cwtsite.com is a nice page."))
-    .toEqual('A page called <a href="https://www.cwtsite.com" target="_blank">https://www.cwtsite.com</a> is a nice page.',
-      'https and www failed')
+    expect(convertLinks("A page called https://www.cwtsite.com is a nice page."))
+        .toEqual('A page called <a href="https://www.cwtsite.com" target="_blank">https://www.cwtsite.com</a> is a nice page.',
+            'https and www failed')
 });
 
 it('containing a link with multiple subdomains.', () => {
-  expect(convertLinks("A page called http://www.2009.cwtsite.com is a nice page."))
-    .toEqual('A page called <a href="http://www.2009.cwtsite.com" target="_blank">http://www.2009.cwtsite.com</a> is a nice page.');
+    expect(convertLinks("A page called http://www.2009.cwtsite.com is a nice page."))
+        .toEqual('A page called <a href="http://www.2009.cwtsite.com" target="_blank">http://www.2009.cwtsite.com</a> is a nice page.');
 });
 
 it('containing a link as the last part of a sentence.', () => {
-  expect(convertLinks("I really like https://cwtsite.com."))
-    .toEqual('I really like <a href="https://cwtsite.com" target="_blank">https://cwtsite.com</a>.');
+    expect(convertLinks("I really like https://cwtsite.com."))
+        .toEqual('I really like <a href="https://cwtsite.com" target="_blank">https://cwtsite.com</a>.');
 });
 
 it('containing a link preceding a comma.', () => {
-  expect(convertLinks("I really like https://cwtsite.com, it's true."))
-    .toEqual('I really like <a href="https://cwtsite.com" target="_blank">https://cwtsite.com</a>, it\'s true.');
+    expect(convertLinks("I really like https://cwtsite.com, it's true."))
+        .toEqual('I really like <a href="https://cwtsite.com" target="_blank">https://cwtsite.com</a>, it\'s true.');
 });
 
 it('containing an email address', () => {
-  expect(convertLinks("When I have question, I write to support@cwtsite.com and get answers."))
-    .toEqual('When I have question, I write to <a href="mailto:support@cwtsite.com">support@cwtsite.com</a> and get answers.');
+    expect(convertLinks("When I have question, I write to support@cwtsite.com and get answers."))
+        .toEqual('When I have question, I write to <a href="mailto:support@cwtsite.com">support@cwtsite.com</a> and get answers.');
 });
 
 it('containing an email address and a link', () => {
-  expect(convertLinks("Emails at support@cwtsite.com and internet at http://cwtsite.com for much fun."))
-    .toEqual('Emails at <a href="mailto:support@cwtsite.com">support@cwtsite.com</a> and internet at <a href="http://cwtsite.com" target="_blank">http://cwtsite.com</a> for much fun.');
+    expect(convertLinks("Emails at support@cwtsite.com and internet at http://cwtsite.com for much fun."))
+        .toEqual('Emails at <a href="mailto:support@cwtsite.com">support@cwtsite.com</a> and internet at <a href="http://cwtsite.com" target="_blank">http://cwtsite.com</a> for much fun.');
 });
+
+it('containing a small sub-domain', () => {
+    expect(convertLinks('Spend your waiting-for-opponent-time with a bit of One Of Everything https://wl.zemke.io/'))
+        .toEqual('Spend your waiting-for-opponent-time with a bit of One Of Everything <a href="https://wl.zemke.io" target="_blank">https://wl.zemke.io</a>/');
+    });
 
 console.log('\x1b[32m%s\x1b[0m', 'Success');
